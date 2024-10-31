47ef17eae3a6977f97d18cc5c83f779d8942802c
Liam <liam14mitchell@gmail.com>
2024-10-31 20:15:24 +0000

    render todos by dynamically creating from project object array

--- src/dom.js
+++ src/dom.js
@@ -19,6 +19,7 @@ function openAddTaskForm() {
 
 function cancelAddTask() {
     document.getElementById("add-task-dialog").open = false;
+    document.getElementById("add-task-form").reset();
 }
 
 function handleCreateTodo() {
@@ -26,7 +27,107 @@ function handleCreateTodo() {
     //close and reset the form
     document.getElementById("add-task-dialog").open = false;
     //render the page to show new task
+    renderPage();
     
+}
+
+function renderPage() {
+
+    const listItemContainer = document.getElementById("list-item-container");
+    //clear the page
+    listItemContainer.innerHTML = "";
+
+    console.log("renderPage");
+
+    // <div class="list-item">
+
+    //                 <button class="check-btn">
+    //                     <img class="unchecked" src="./media/button-unchecked.svg">
+    //                     <img class="checked" src="./media/button-checked.svg">
+    //                 </button>
+
+    //                 <div class="list-item-text">
+    //                     <p class="list-item-title">Task 1</p>
+    //                     <p class="list-item-description">whimmy wham wham wazzle</p>
+    //                     <div class="date-container">
+    //                         <img src="./media/calendar-today.svg">
+    //                         <p class=" date due-today">Today</p>
+    //                     </div>
+
+    //                 </div>
+
+    //             </div>
+
+    //loop through default projects todoArr
+    //create html elements with the data in the todoArr
+
+    for(let i = 0; i < defaultProject.todoArr.length; i++){
+
+        const listItemDiv = document.createElement("div");
+        listItemDiv.classList = "list-item";
+
+        const listItemBtn = document.createElement("button");
+        listItemBtn.classList = "check-btn";
+        listItemDiv.appendChild(listItemBtn);
+
+        const uncheckedImg = document.createElement("img");
+        uncheckedImg.src = "./media/button-unchecked.svg";
+        uncheckedImg.classList = "unchecked";
+        listItemBtn.appendChild(uncheckedImg);
+
+        const checkedImg = document.createElement("img");
+        checkedImg.src = "./media/button-checked.svg";
+        checkedImg.classList = "checked";
+        listItemBtn.appendChild(checkedImg);
+
+        const listItemTextDiv = document.createElement("div");
+        listItemTextDiv.classList = "list-item-text";
+        listItemDiv.appendChild(listItemTextDiv);
+
+        const listItemTitle = document.createElement("p");
+        listItemTitle.classList = "list-item-title";
+        const listItemTitleNode = document.createTextNode(defaultProject.todoArr[i].taskName.value);
+        listItemTitle.appendChild(listItemTitleNode);
+        listItemTextDiv.appendChild(listItemTitle);
+
+        const listItemDesc = document.createElement("p");
+        listItemDesc.classList = "list-item-description";
+        const listItemDescNode = document.createTextNode(defaultProject.todoArr[i].taskDesc.value);
+        listItemDesc.appendChild(listItemDescNode);
+        listItemTextDiv.appendChild(listItemDesc);
+
+        const dateContainer = document.createElement("div");
+        dateContainer.classList = "date-container";
+        listItemTextDiv.appendChild(dateContainer);
+
+        const dateImg = document.createElement("img");
+        dateImg.src = "./media/calendar-today.svg";
+        dateContainer.appendChild(dateImg);
+
+        const dateText = document.createElement("p");
+        dateText.classList = "date due-today";
+        const dateTextNode = document.createTextNode("Today maybe");
+        dateText.appendChild(dateTextNode);
+        dateContainer.appendChild(dateText);
+
+        listItemContainer.appendChild(listItemDiv);
+
+        const horizRule = document.createElement("hr");
+        listItemContainer.appendChild(horizRule);
+
+
+
+
+
+
+
+        
+
+
+
+        
+    }
+
 }
 
 export default setUpEventListeners;
--- src/project.js
+++ src/project.js
@@ -12,11 +12,13 @@ class Project {
         const taskName = document.getElementById("form-task-name");
         const taskDesc = document.getElementById("form-task-description");
         const taskDueDate = document.getElementById("form-task-date");
-        //create todo class with data
-        const todo = new ToDo(taskName, taskDesc, taskDueDate);
-        //push object to todoArr
-        this.todoArr.push(todo);
-        console.log(todo.taskName.value);
+        //check form is valid
+        if (taskName.value != "") {
+            //create todo class with data
+            const todo = new ToDo(taskName, taskDesc, taskDueDate);
+            //push object to todoArr
+            this.todoArr.push(todo);
+        }
     }
 
 }
